fab948f080c31a991c3ae56dc0d844ad28a4e46f
araujolucasldas <araujo.lucas.ldas@gmail.com>
2023-11-08 21:14:02 -0300

    "Tarefa 1 concluída"

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,27 @@
-/* Desenvolva sua lógica aqui ... */
+const button = document.querySelector(".header__btn")
+const html = document.querySelector("html")
+let darkMode = false
+
+function themeChange(){
+    darkMode = !darkMode
+
+    html.classList.toggle("dark-mode")
+    button.classList.toggle("header__btn--dark-mode")
+
+    localStorage.setItem("theme", JSON.stringify(darkMode))
+}
+
+button.addEventListener("click", themeChange)
+
+//localStorage.setItem("theme", JSON.stringify(darkMode))
+
+
+function themeAnalysis(){
+    darkMode = JSON.parse(localStorage.getItem("theme"))
+
+    if(darkMode){
+        html.classList.add("dark-mode")
+        button.classList.add("header__btn--dark-mode")
+    }
+}
+themeAnalysis()
